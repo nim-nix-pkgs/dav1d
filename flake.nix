e504ea89bfe7ef9d51465ff952cd4e64d19ba059
hugosenari <hugosenari@gmail.com>
2022-03-12 02:35:41 -0300

    chore: re index dav1d

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = ''A nimterop wrapper for the dav1d portable-and-fast AV1 video decoder'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."dav1d-master".url = "path:./master";
+  inputs."dav1d-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."dav1d-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
